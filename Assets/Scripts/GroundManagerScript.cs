65992e36de978899317d495b19245cc5c83b8509
LD9-RYOHEI-KITO\Ryohei <tagatame1996@yahoo.co.jp>
2017-09-25 21:20:31 +0900

    first commit

---
+++ Assets/Scripts/GroundManagerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundManagerScript : MonoBehaviour {
+
+	[SerializeField] GameObject groudPrefab;
+	[SerializeField] GameObject prevObj;
+
+	void Update () {
+		if (prevObj.transform.position.x < -10) {
+			prevObj = Instantiate (groudPrefab, new Vector3(39, -4 , 0), Quaternion.identity) as GameObject;
+		}
+
+		if (prevObj.transform.position.x < -38) {
+			Destroy (prevObj);
+		}
+
+	}
+}
---
+++ Assets/Scripts/GroundScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundScript : MonoBehaviour {
+
+	public GameObject ground;
+	public GameObject player;
+
+	float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position += new Vector3 (-speed * Time.deltaTime, 0, 0);
+
+	}
+}
