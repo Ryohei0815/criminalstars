413250f7163f351977727bcd9ef04813dba3f666
Ryohei0815 <lifeistech@lit-mac-3.local>
2017-09-27 18:36:37 +0900

    Add BackGround and Ground Texture and 1probrem

---
+++ Assets/Scripts/BarrierScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierScript : MonoBehaviour {
+
+	public GameObject barrier;
+	int num = Randam.Range(0,3);
+
+
+	void Awake(){
+		if (num = 0) {
+			transform.position = new Vector3 (Randam.Range(14,62),0,0);
+			Instantiate (barrier,transform.position,Quaternion.identity);
+		}
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
