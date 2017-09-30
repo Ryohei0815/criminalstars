73d7795e12288e41a1a76bf33276d8d952f9c0d4
Ryohei0815 <tagatame1996@yahoo.co.jp>
2017-09-30 18:34:11 +0900

    make barrier

--- Assets/Scripts/BarrierScript.cs
+++ Assets/Scripts/BarrierScript.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class BarrierScript : MonoBehaviour {
 
-	public GameObject barrier;
-	int num = Randam.Range(0,3);
-
+	public GameObject refObj;
+	GameObject prevObj;
+	public GameObject barrierObj;
+	int num;
+	GroundManagerScript d1;
+	float [] brriPosiX = new float[4];
 
 	void Awake(){
-		if (num = 0) {
-			transform.position = new Vector3 (Randam.Range(14,62),0,0);
-			Instantiate (barrier,transform.position,Quaternion.identity);
+		num = Random.Range(1,5);
+		Debug.Log (num);
+		refObj = GameObject.Find ("GroundCreater");
+		d1 = refObj.GetComponent<GroundManagerScript> ();
+
+		if (this.transform.parent.name == "Ground"){
+			num = 0;
 		}
-		
 	}
 	
 
 	void Update () {
-		
+		prevObj = d1.ReturnPrevObj();
+
+		if (num >= 1) {
+			transform.position = new Vector3 (Random.Range(14f,62f),-2.25f,0);
+			barrierObj = Instantiate (barrierObj,transform.position,Quaternion.identity) as GameObject;
+			//barrierObj.transform.parent = prevObj.transform;
+			barrierObj.transform.SetParent(prevObj.transform.localScale, false);
+			num--;
+			Debug.Log ("Instantiate");
+		}
 	}
+
+
 }
--- Assets/Scripts/GroundManagerScript.cs
+++ Assets/Scripts/GroundManagerScript.cs
@@ -8,8 +8,10 @@ public class GroundManagerScript : MonoBehaviour {
 	[SerializeField] GameObject prevObj;
 
 	void Update () {
+		//Debug.Log (prevObj.transform.position.x);
 		if (prevObj.transform.position.x < -10) {
 			prevObj = Instantiate (groundPrefab, new Vector3(39, -4 , 0), Quaternion.identity) as GameObject;
+			//Debug.Log ("Instansiate");
 		}
 
 		if (prevObj.transform.position.x < -38) {
@@ -17,4 +19,8 @@ public class GroundManagerScript : MonoBehaviour {
 		}
 
 	}
+	public GameObject ReturnPrevObj (){
+		return prevObj;
+	}
+
 }
