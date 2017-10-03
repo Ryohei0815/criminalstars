6aae65a573947b55767df35f06756dfdce4e0746
Ryohei0815 <tagatame1996@yahoo.co.jp>
2017-10-03 22:08:11 +0900

    障害物、市民、ゲームオーバーの追加

---
+++ Assets/Scripts/BarrierManagerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierManagerScript : MonoBehaviour {
+
+	//public GameObject refObj;
+	//GameObject prevObj;
+	//GroundManagerScript d1;
+	//public GameObject barrierObj;
+	public GameObject[] barrierObjs;
+
+	private int num;
+
+	void Awake() {
+		num = Random.Range(1, barrierObjs.Length);
+		List<int> tempNums = new List<int>() { 0, 1, 2, 3, 4 };
+		//refObj = GameObject.Find("GroundCreater");
+		//d1 = refObj.GetComponent<GroundManagerScript>();
+
+		for (int i = 0; i < num; i++) {
+			int randomIndex = Random.Range(0, tempNums.Count);
+			barrierObjs[tempNums[randomIndex]].SetActive(true);
+			tempNums.Remove(randomIndex);
+		}
+
+
+		if (this.transform.parent.name == "Ground") {
+			num = 0;
+		}
+	}
+
+
+	void Update() {
+		//prevObj = d1.ReturnPrevObj();
+
+		//if (num >= 1) {
+		//	//transform.position = new Vector3(Random.Range(14f, 62f), -2.25f, 0);
+		//	//var obj = Instantiate(barrierObj, prevObj.transform) as GameObject;
+		//	var obj = Instantiate(barrierObj) as GameObject;
+		//	//obj.transform.position = brriPos[Random.Range(0, 4)].position;
+		//	//barrierObj.transform.parent = prevObj.transform;
+		//	obj.transform.SetParent(prevObj.transform, false);
+		//	obj.GetComponent<BoxCollider>().isTrigger = false;
+		//	num--;
+		//	Debug.Log("Instantiate");
+		//}
+	}
+
+
+}
---
+++ Assets/Scripts/CitizenManagerScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CitizenManagerScript : MonoBehaviour {
+	
+	public GameObject[] citizenObjs;
+
+	private int num;
+
+	void Awake() {
+		num = Random.Range(1, citizenObjs.Length);
+		List<int> tempNums = new List<int>() { 0, 1, 2, 3 };
+		//refObj = GameObject.Find("GroundCreater");
+		//d1 = refObj.GetComponent<GroundManagerScript>();
+
+		for (int i = 0; i < num; i++) {
+			int randomIndex = Random.Range(0, tempNums.Count);
+			citizenObjs[tempNums[randomIndex]].SetActive(true);
+			tempNums.Remove(randomIndex);
+		}
+
+
+		if (this.transform.parent.name == "Ground") {
+			num = 0;
+		}
+	}
+}
