1554bde46252837ed2a74ebd483cd9283dd30336
ryosebach <ryo.freezia@gmail.com>
2017-10-03 19:38:25 +0900

    barrier機能の修正を加えた

--- Assets/Scripts/BarrierScript.cs
+++ Assets/Scripts/BarrierScript.cs
@@ -4,36 +4,47 @@ using UnityEngine;
 
 public class BarrierScript : MonoBehaviour {
 
-	public GameObject refObj;
-	GameObject prevObj;
+	//public GameObject refObj;
+	//GameObject prevObj;
+	//GroundManagerScript d1;
 	public GameObject barrierObj;
-	int num;
-	GroundManagerScript d1;
-	float [] brriPosiX = new float[4];
+	public GameObject[] barrierObjs;
 
-	void Awake(){
-		num = Random.Range(1,5);
-		Debug.Log (num);
-		refObj = GameObject.Find ("GroundCreater");
-		d1 = refObj.GetComponent<GroundManagerScript> ();
+	private int num;
 
-		if (this.transform.parent.name == "Ground"){
+	void Awake() {
+		num = Random.Range(1, barrierObjs.Length);
+		List<int> tempNums = new List<int>() { 0, 1, 2, 3, 4 };
+		//refObj = GameObject.Find("GroundCreater");
+		//d1 = refObj.GetComponent<GroundManagerScript>();
+
+		for (int i = 0; i < num; i++) {
+			int randomIndex = Random.Range(0, tempNums.Count);
+			barrierObjs[tempNums[randomIndex]].SetActive(true);
+			tempNums.Remove(randomIndex);
+		}
+
+
+		if (this.transform.parent.name == "Ground") {
 			num = 0;
 		}
 	}
-	
 
-	void Update () {
-		prevObj = d1.ReturnPrevObj();
 
-		if (num >= 1) {
-			transform.position = new Vector3 (Random.Range(14f,62f),-2.25f,0);
-			barrierObj = Instantiate (barrierObj,transform.position,Quaternion.identity) as GameObject;
-			//barrierObj.transform.parent = prevObj.transform;
-			barrierObj.transform.SetParent(prevObj.transform.localScale, false);
-			num--;
-			Debug.Log ("Instantiate");
-		}
+	void Update() {
+		//prevObj = d1.ReturnPrevObj();
+
+		//if (num >= 1) {
+		//	//transform.position = new Vector3(Random.Range(14f, 62f), -2.25f, 0);
+		//	//var obj = Instantiate(barrierObj, prevObj.transform) as GameObject;
+		//	var obj = Instantiate(barrierObj) as GameObject;
+		//	//obj.transform.position = brriPos[Random.Range(0, 4)].position;
+		//	//barrierObj.transform.parent = prevObj.transform;
+		//	obj.transform.SetParent(prevObj.transform, false);
+		//	obj.GetComponent<BoxCollider>().isTrigger = false;
+		//	num--;
+		//	Debug.Log("Instantiate");
+		//}
 	}
 
 
